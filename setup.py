e790093ef3a3a02db97da10344ef82ee4e31e1a7
gaopl <gaopl@jiedaibao.com>
2017-08-15 17:22:11 +0800

    add setup.py!

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pythn
+
+from setuptools import setup, find_packages
+
+setup(
+    name='aioetcd3',
+    version="0.1",
+    install_requires = [],
+    use_2to3=False,
+    packages=find_packages()
+)

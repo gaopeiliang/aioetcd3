07381036c809bedd05487a14b6f6f29ecbf76a78
gaopl <gaopl@jiedaibao.com>
2017-08-28 18:07:33 +0800

    fix watch interface!

--- aioetcd3/watch.py
+++ aioetcd3/watch.py
@@ -2,8 +2,10 @@ import asyncio
 
 from aioetcd3.base import StubMixin
 import aioetcd3.rpc.rpc_pb2 as rpc
+import aioetcd3.rpc.kv_pb2 as kv
 from asyncio.queues import Queue
 from aioetcd3.utils import put_key_range
+from aioetcd3.kv import KVMetadata
 
 DEFAUTL_INPUT_QUEUE_LIMIT = 10000
 
@@ -16,6 +18,28 @@ class WatchResponseMeta(object):
         self.compact_revision = response.compact_revision
         self.cancel_reason = response.cancel_reason
 
+    def __str__(self):
+        return str({"watch_id": self.watch_id, "created": self.created,
+                    "canceled": self.canceled, "compact_revision": self.compact_revision,
+                    "cancel_reason": self.cancel_reason})
+
+
+class Event(object):
+    def __init__(self, event):
+        self.type = event.type
+
+        if self.type == kv.Event.EventType.PUT and event.kv.Version == 1:
+            self.type = 'created'
+
+        self.key = event.kv.key
+        self.value = event.kv.value
+        self.meta = KVMetadata(event.kv)
+
+        self.pre_value = event.prev_kv.value
+        self.pre_meta = KVMetadata(event.prev_kv)
+
+    def __str__(self):
+        return f'{self.type} {self.key},{self.value}'
 
 class Watch(StubMixin):
     def _update_channel(self, channel):
@@ -39,16 +63,15 @@ class Watch(StubMixin):
         async with self._watch_stub.Watch.with_scope(request_iter) as response_iter:
 
             async for response in response_iter:
-                request, out_queue = self._pending_created.pop(0)
                 if response.created:
+                    request, out_queue = self._pending_created.pop(0)
                     self._watch_listening[response.watch_id] = (request, out_queue)
                     self._watch_id[out_queue] = response.watch_id
 
                 if response.watch_id in self._watch_listening:
                     _, out_q = self._watch_listening[response.watch_id]
 
-                    for e in response.events:
-                        await out_q.put((e, WatchResponseMeta(response)))
+                    await out_q.put((response.events, WatchResponseMeta(response)))
 
                 if response.canceled:
                     if response.watch_id in self._watch_listening:
@@ -70,15 +93,17 @@ class Watch(StubMixin):
                 yield request
 
         while True:
-            request, out_queue = await self._input_queue.get()
+            is_created, request, out_queue = await self._input_queue.get()
             if request is None and out_queue is None:
                 # stop iter request , stop watch stream
                 break
-            self._pending_created.append((request, out_queue))
+            if is_created:
+                self._pending_created.append((request, out_queue))
+
             yield request
 
     async def stop_task(self):
-        await self._input_queue.put((None, None))
+        await self._input_queue.put((None, None, None))
 
     async def watch(self, key_range, start_revision=None, noput=False, nodelete=False, prev_kv=False):
 
@@ -93,40 +118,50 @@ class Watch(StubMixin):
                                                prev_kv=prev_kv)
 
         put_key_range(watch_request, key_range)
+        request = rpc.WatchRequest(create_request=watch_request)
 
         response_queue = Queue()
+        await self._input_queue.put((True, request, response_queue))
 
-        async def event_iter(queue):
-            while True:
-                event, meta = await queue.get()
-                if event is None and meta is None:
-                    break
-                yield event
-
-        await self._input_queue.put((watch_request, response_queue))
-
-        return response_queue, event_iter(response_queue)
-
-    async def unwatch(self, unique_queue):
-
-        max_time = 5
         while True:
-            queue_info = [q for _, q in self._pending_created]
-            if unique_queue not in queue_info:
+            events, meta = await  response_queue.get()
+            if events is None:
                 break
-            else:
-                max_time = max_time - 1
-                await asyncio.sleep(1)
 
-            if max_time <= 0:
-                raise ValueError("watch request mybe have no response")
+            for e in events:
+                yield (Event(e), meta)
 
-        if unique_queue not in self._watch_id:
-            raise ValueError("find watch id error")
+        # async def event_iter(queue):
+        #     while True:
+        #         event, meta = await queue.get()
+        #         if event is None and meta is None:
+        #             break
+        #         yield event
+        #
+        # await self._input_queue.put((watch_request, response_queue))
+        #
+        # return response_queue, event_iter(response_queue)
 
-        watch_id = self._watch_id[unique_queue]
-
-        request = rpc.WatchCancelRequest(watch_id=watch_id)
-        response_queue = Queue()
-
-        await self._input_queue.put((request, response_queue))
+    # async def unwatch(self, unique_queue):
+    #
+    #     max_time = 5
+    #     while True:
+    #         queue_info = [q for _, q in self._pending_created]
+    #         if unique_queue not in queue_info:
+    #             break
+    #         else:
+    #             max_time = max_time - 1
+    #             await asyncio.sleep(1)
+    #
+    #         if max_time <= 0:
+    #             raise ValueError("watch request mybe have no response")
+    #
+    #     if unique_queue not in self._watch_id:
+    #         raise ValueError("find watch id error")
+    #
+    #     watch_id = self._watch_id[unique_queue]
+    #
+    #     request = rpc.WatchCancelRequest(watch_id=watch_id)
+    #     response_queue = Queue()
+    #
+    #     await self._input_queue.put((request, response_queue))

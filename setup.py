11e8c31fe24e74a1a6f173de0645ad4a68f0692f
gaopl <gaopl@jiedaibao.com>
2017-09-11 17:26:19 +0800

    fix setup.py!

--- setup.py
+++ setup.py
@@ -1,11 +1,41 @@
-#!/usr/bin/env pythn
+#!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
+version = "1.0"
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ""
+
 
 setup(
-    name='aioetcd3',
-    version="0.1",
-    install_requires = [],
-    use_2to3=False,
-    packages=find_packages()
+    name="aioetcd3",
+    version=version,
+    author="gaopeiliang",
+    author_email="964911957@qq.com",
+    long_description=long_description,
+    description="asyncio wrapper for etcd v3",
+    license="Apache",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Framework :: AsyncIO',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Framework :: AsyncIO',
+    ],
+    url="https://github.com/gaopeiliang/aioetcd3",
+    platforms=['any'],
+    packages=[
+        'aioetcd3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'aiogrpc'
+    ]
 )
